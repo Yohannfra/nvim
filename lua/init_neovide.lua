590e85ef1892b3a64b733645aebd2d76431fb322
Yohann Assouline <yohannassouline@hotmail.fr>
2022-10-24 02:20:55 +0200

    Disable tmux shortcuts in neovide

--- lua/init_neovide.lua
+++ lua/init_neovide.lua
@@ -8,6 +8,12 @@ if vim.g.neovide then
     vim.fn.execute(":cd")
 end
 
+-- Disable tmux shortcuts
 map('n', '<C-b>n', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>c', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>s', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>v', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>l', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>h', ':echo "Use alt+tab to switch to terminal"<CR>')
 
 vim.opt.guifont = { "JetBrainsMono Nerd Font Mono", "h13" }

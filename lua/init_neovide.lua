bcaf312ce73a6ef4f87b741b1244c5f3d9513871
Yohann Assouline <yohannassouline@hotmail.fr>
2022-11-20 17:31:22 +0100

    update neovide config

--- lua/init_neovide.lua
+++ lua/init_neovide.lua
@@ -15,8 +15,11 @@ map('n', '<C-b>s', ':echo "Use alt+tab to switch to terminal"<CR>')
 map('n', '<C-b>v', ':echo "Use alt+tab to switch to terminal"<CR>')
 map('n', '<C-b>l', ':echo "Use alt+tab to switch to terminal"<CR>')
 map('n', '<C-b>h', ':echo "Use alt+tab to switch to terminal"<CR>')
+map('n', '<C-b>m', ':echo "Use alt+tab to switch to terminal"<CR>')
 map('n', '<C-b><Esc>', ':echo "Use alt+tab to switch to terminal"<CR>')
 
 vim.opt.guifont = { "JetBrainsMono Nerd Font Mono", "h13" }
 
 vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_input_use_logo = 1
+vim.g.neovide_cursor_vfx_mode = "ripple"
--- lua/settings.lua
+++ lua/settings.lua
@@ -110,14 +110,10 @@ opt.ttimeoutlen = 100
 -- enable rainbow brackets
 vim.api.nvim_set_var('rainbow_active', 1)
 
--- for neovide
 vim.cmd [[
 " Allow copy paste in neovim
-let g:neovide_input_use_logo = 1
 map <D-v> "+p<CR>
 map! <D-v> <C-R>+
 tmap <D-v> <C-R>+
 vmap <D-c> "+y<CR> 
-
-let g:neovide_cursor_vfx_mode = "ripple"
 ]]

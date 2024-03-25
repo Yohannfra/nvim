c89ce249e1799ed65f20049b2ecdd7f3b120d20c
Yohann Assouline <yohannassouline@hotmail.fr>
2024-03-25 15:40:42 +0100

    Update autocmds.lua
    Add some comments and disable some syntaxes that are now handled with treesitter

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,6 +1,4 @@
--- Strip whitespace when save
--- vim.cmd('autocmd BufWritePre * %s/\\s\\+$//e')
-
+-- Delete trailing whitespaces on save
 -- vim.api.nvim_create_autocmd('BufWritePre', {pattern="*", command='%s/\\s\\+$//e'})
 
 -- Comment string for asm files
@@ -10,30 +8,27 @@ vim.api.nvim_create_autocmd('FileType', {pattern="c", command='setlocal comments
 -- Comment string for .Xressources
 vim.api.nvim_create_autocmd('FileType', {pattern="xdefaults", command='setlocal commentstring=!\\ %s'})
 
--- Highlight _t in c and in cpp
-vim.cmd [[
-augroup highlight_t_in_c
-    autocmd!
-    autocmd Syntax c,cpp syntax match cStructure /\w*_t\s/
-    autocmd Syntax c,cpp syntax match cStructure /\w*_t;/
-    autocmd Syntax c,cpp syntax match cStructure /\w*_t)/
+-- Highlight _t (for structures) in c and in cpp
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_t\\s/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_t;/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_t)/'})
 
-    autocmd Syntax c,cpp syntax match cStructure /\w*_e\s/
-    autocmd Syntax c,cpp syntax match cStructure /\w*_e;/
-    autocmd Syntax c,cpp syntax match cStructure /\w*_e)/
+-- Highlight _e (for enums) in c and in cpp
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_e\\s/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_e;/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cStructure /\\w*_e)/'})
 
-     autocmd Syntax c,cpp syntax match cType /u8/
-     autocmd Syntax c,cpp syntax match cType /i8/
-     autocmd Syntax c,cpp syntax match cType /u16/
-     autocmd Syntax c,cpp syntax match cType /i16/
-     autocmd Syntax c,cpp syntax match cType /u32/
-     autocmd Syntax c,cpp syntax match cType /i32/
-     autocmd Syntax c,cpp syntax match cType /u64/
-     autocmd Syntax c,cpp syntax match cType /i64/
-     autocmd Syntax c,cpp syntax match cType /f32/
-     autocmd Syntax c,cpp syntax match cType /f64/
-augroup end
-]]
+-- Highlight short types in c and in cpp
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /u8/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /i8/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /u16/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /i16/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /u32/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /i32/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /u64/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /i64/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /f32/'})
+-- vim.api.nvim_create_autocmd('FileType', {pattern="c,cpp", command='syntax match cType /f64/'})
 
 -- highlight yank
 vim.api.nvim_create_autocmd('TextYankPost', {pattern="*",
@@ -52,13 +47,23 @@ autocmd BufReadPost *
 augroup END
 ]]
 
--- a few syntax
+-- Segger embedded studio build files
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern="*.emProject", command='set filetype=xml'})
-vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern=".env.test", command='set filetype=sh'})
+
+-- .mm is the extension for the C++ compilation unit expected by the Objective-C (macos stuff)
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern="*.mm", command='set filetype=cpp'})
+
+-- the overlay files in Zephyr RTOS
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern="*.overlay", command='set filetype=dts'})
+
+-- the config file for pymakr pycom
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern="pymakr.conf", command='set filetype=json'})
+
+--- the config file in ESP_IDF
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern="sdkconfig.defaults", command='set filetype=conf'})
+
+-- for commons .env files
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern=".env.test", command='set filetype=sh'})
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern=".env.local", command='set filetype=sh'})
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern=".env.template", command='set filetype=sh'})
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {pattern=".env.dev", command='set filetype=sh'})

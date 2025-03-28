a9171775d39415efb37664ee8de15543a7af1859
Yohann Assouline <yohannassouline@hotmail.fr>
2025-03-28 11:03:03 +0100

    update vimwiki config

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -1,8 +1,16 @@
 vim.g.vimwiki_list = {
   {
     path = '~/vimwiki/',
-    path_html = '~/vimwiki/public_html/',
+    path_html = '~/vimwiki/html/',
   }
 }
 
 vim.g.vimwiki_global_ext = 0
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.wiki" },
+  callback = function()
+    vim.cmd("silent! VimwikiAll2HTML")
+  end,
+  group = vim.api.nvim_create_augroup("VimwikiAutoExport", { clear = true }),
+})
